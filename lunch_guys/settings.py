56610263e80c5da72934eb8c3eb16abc9f5ab89a
sarahlynch <sarahlynch@users.noreply.github.com>
2017-08-17 11:27:35 +1000

    added true database details

--- lunch_guys/settings.py
+++ lunch_guys/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'lunch_guys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lunch_guys_db',
+        'USER': 'u_lunch_guys',
+        'PASSWORD': 'hungry',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
